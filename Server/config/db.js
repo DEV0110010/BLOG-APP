593f5710d272f02e534c00d5836f2ce06273ed6c
DEV0110010 <newera.deepanshu@gmail.com>
2025-03-18 14:36:36 +0530

    commit

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectToMongo = async () => {
-    const res = await mongoose.connect("mongodb+srv://ProjectUser:Minor%40Project%401234%21%40@project-web-cluster.ujyn0.mongodb.net/?retryWrites=true&w=majority&appName=PROJECT-WEB-CLUSTER/Minor-Project-Blog")
+    const res = await mongoose.connect("mongodb+srv://ProjectUser:Minor%40Project%401234%21%40@project-web-cluster.ujyn0.mongodb.net/?retryWrites=true&w=majority&appName=PROJECT-WEB-CLUSTER/database")
     if(res){
         console.log("Connected Successfully");
     }
